01557ac3b68bc7f9e39e76d4b4bf2fdbe627ce3d
Viktor Engelmann <Viktor.Engelmann@TU-Berlin.de>
2016-07-04 16:23:21 +0200

    Files for part 5, https://www.youtube.com/watch?v=AgeX-U4dKSs

--- kernel.cpp
+++ kernel.cpp
@@ -1,13 +1,14 @@
 
 #include "types.h"
 #include "gdt.h"
+#include "interrupts.h"
 
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     static uint8_t x=0,y=0;
-
+    
     for(int i = 0; str[i] != '\0'; ++i)
     {
         switch(str[i])
@@ -21,13 +22,13 @@ void printf(char* str)
                 x++;
                 break;
         }
-
+        
         if(x >= 80)
         {
             x = 0;
             y++;
         }
-
+        
         if(y >= 25)
         {
             for(y = 0; y < 25; y++)
@@ -57,6 +58,8 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     printf("Hello World! --- http://www.AlgorithMan.de");
 
     GlobalDescriptorTable gdt;
+    InterruptManager interrupts(0x20, &gdt);
+    interrupts.Activate();
 
     while(1);
 }
--- makefile
+++ makefile
@@ -6,9 +6,12 @@ GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o port.o kernel.o
+objects = loader.o gdt.o port.o interruptstubs.o interrupts.o kernel.o
 
 
+run: mykernel.iso
+	(killall VirtualBox && sleep 1) || true
+	VirtualBox --startvm 'My Operating System' &
 
 %.o: %.cpp
 	gcc $(GCCPARAMS) -c -o $@ $<
@@ -33,10 +36,6 @@ mykernel.iso: mykernel.bin
 	echo '}'                                 >> iso/boot/grub/grub.cfg
 	grub-mkrescue --output=mykernel.iso iso
 	rm -rf iso
-
-run: mykernel.iso
-	(killall VirtualBox && sleep 1) || true
-	VirtualBox --startvm 'My Operating System' &
 
 install: mykernel.bin
 	sudo cp $< /boot/mykernel.bin
